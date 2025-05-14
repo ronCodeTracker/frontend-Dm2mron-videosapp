b849f9d850ad737d52b8ab7e5064fded68e69475
Ronald K <2fixelectronics777@gmail.com>
2025-05-14 11:52:17 -0500

    config mod

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
 
+
+
+export default {
+	kit: {
+		// default options are shown
+		adapter: adapter({
 			// if true, will create a Netlify Edge Function rather
 			// than using standard Node-based functions
 			edge: false,
@@ -20,5 +20,3 @@ const config = {
 		})
 	}
 };
-
-export default config;
